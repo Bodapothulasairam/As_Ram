7da89d6910884f158ed60041a334a74cbeead6fd
Bodapothulasairam <sairambodapothula@gmail.com>
2024-10-30 15:42:58 -0500

    New design with only navbar

--- src/app/Common/navbar/navbar.component.ts
+++ src/app/Common/navbar/navbar.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { SharedServicesService } from '../../shared-services.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
-  constructor(private sideNavService: SharedServicesService) {}
+  showImage: boolean = false;
+  constructor(private sideNavService: SharedServicesService) { }
 
-  toggleSideNav() {
-    this.sideNavService.toggleSideNav();
+  public toggleNav(){
+    const sideNav = document.getElementById('sideNav') as HTMLElement; // Type assertion
+    if (sideNav) {
+      sideNav.classList.toggle('active');
+      this.showImage = !this.showImage;
+      console.log(this.showImage);
+    }
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,16 +2,15 @@ import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { NavbarComponent } from "../Common/navbar/navbar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatGridListModule,MatCardModule, MatButtonModule],
+  imports: [MatGridListModule, MatCardModule, MatButtonModule, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  centered = false;
-  disabled = false;
-  unbounded = false;
+  
 }
