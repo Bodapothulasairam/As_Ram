f19b236ed3845728c30fda3574db20a6ec8629d7
Bodapothulasairam <sairambodapothula@gmail.com>
2024-10-31 20:29:37 -0500

    Added buttons and profile image

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { NavbarComponent } from "../Common/navbar/navbar.component";
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatChipsModule} from '@angular/material/chips';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatGridListModule, MatCardModule, MatButtonModule, NavbarComponent],
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule,MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  greetingText: string="Hi, Myself";
+  Name: string = "Sairam Bodapothula";
+  designation: string = "And I'm a Full-stack Developer";
+  overview="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde dolores molestiae quis nisi animi aliquam quia possimus quaerat. Obcaecati modi sequi voluptate dignissimos suscipit, voluptas dolorem totam aut minus aliquam! Quae asperiores corporis magni perferendis."
+  longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
+  Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
+  usually kept as a companion animal or for showing.`;
   constructor() {
   }
-  
 }
